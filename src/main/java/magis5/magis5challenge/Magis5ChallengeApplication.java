cc6cdf2256ddbfb1fe08ab4a99c943e2ea317861
Renan Fermino <renan.fermino@magis5.com.br>
2025-05-03 13:37:38 -0300

    first commit

---
+++ src/main/java/magis5/magis5challenge/Magis5ChallengeApplication.java
@@ -0,0 +1,13 @@
+package magis5.magis5challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Magis5ChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Magis5ChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/magis5/magis5challenge/Magis5ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package magis5.magis5challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Magis5ChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
