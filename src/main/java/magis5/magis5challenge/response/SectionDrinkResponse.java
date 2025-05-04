a7895b0740a0c78e57de68a4341aec4a8fafe5dd
Renan Fermino <renan.fermino@magis5.com.br>
2025-05-03 22:36:58 -0300

    refactor: SectionDrinkResponse to use camel case

--- src/main/java/magis5/magis5challenge/response/SectionDrinkResponse.java
+++ src/main/java/magis5/magis5challenge/response/SectionDrinkResponse.java
@@ -1,5 +1,6 @@
 package magis5.magis5challenge.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,8 +16,15 @@ import magis5.magis5challenge.enumeration.EDrinkType;
 public class SectionDrinkResponse {
   private UUID id;
   private BigDecimal stock;
+
+  @JsonProperty("drink_type")
   private EDrinkType drinkType;
+
+  @JsonProperty("created_at")
   private LocalDateTime createdAt;
+
+  @JsonProperty("updated_at")
   private LocalDateTime updatedAt;
+
   private List<DrinkGetResponse> drinks;
 }
